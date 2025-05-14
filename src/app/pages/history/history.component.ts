17c5b61093745c4d747a76db727d6950a74505e5
Henry <henrydiazgutierrez12@gmail.com>
2025-05-13 19:44:45 -0500

    reschedule

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import {NgIf, NgOptimizedImage} from '@angular/common';
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [
+    NgIf
+  ]
+  ,
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
 export class HistoryComponent {
-
+  showPopup = false;
+  openPopup(): void {
+    this.showPopup = true;
+    document.body.style.overflow = 'hidden';
+  }
+  closePopup(): void {
+    this.showPopup = false;
+    document.body.style.overflow = 'auto';
+  }
 }
+
